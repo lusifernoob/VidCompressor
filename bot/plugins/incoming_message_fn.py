51f1ad11b703fbe9d115326cf3bebd81fb7b00b7
vasusen-code <84653476+vasusen-code@users.noreply.github.com>
2021-07-12 17:50:31 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K / Akshay C / @AbirHasan2005
+# (c) Shrimadhav U K / Akshay C / @AbirHasan2005 / @Chauhanmahesh
 
 # the logging things
 
